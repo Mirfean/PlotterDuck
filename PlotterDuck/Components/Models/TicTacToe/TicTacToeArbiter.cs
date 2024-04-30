89c227b25f2cf6a59168ecabe08182f649704720
Mirfean <mrfruzio@gmail.com>
2024-04-30 14:32:21 +0200

    minor changes to TicTacToe

--- PlotterDuck/Components/Models/TicTacToe/TicTacToeArbiter.cs
+++ PlotterDuck/Components/Models/TicTacToe/TicTacToeArbiter.cs
@@ -59,19 +59,21 @@ namespace PlotterDuck.Components.Models.TicTacToe
 
 		#region BOT
 
-		public int? GetBestMove()
+		public int? GetBestMove(int botLevel)
         {
             //Check for win
-            List<int> bestMoves = All1toWin(TTTBoxState.O);
+            List<int> winningMove = All1toWin(TTTBoxState.O);
             List<int> movesToBlock = All1toWin(TTTBoxState.X);
-			//Best bot
-			if (bestMoves.Count > 0)
+			
+            //Best bot
+			if (winningMove.Count > 0)
 			{
-				var bestMove = from move in bestMoves
+				var bestMove = from move in winningMove
 							   orderby move
 							   select move;
                 return bestMove.First();
 			}
+
             //Block player
             if(movesToBlock.Count > 0)
             {
@@ -81,12 +83,25 @@ namespace PlotterDuck.Components.Models.TicTacToe
 				return bestMove.First();
             }
 
-            int? result = GetRandomAvailable();
+            //Random move by level
+            int? result;
+            if(botLevel <= 1)
+            {
+				result = GetRandomAvailable();
+			}
+            else
+            {
+                result = GetBestAvailableFromList(new int[] { 0, 2, 4, 6, 8 });
+            }
             if(result != null)
             {
                 return result;
             }
-            return null;
+            else
+            {
+                result = GetBestAvailableFromList(new int[] { 1, 3, 5, 7 });
+            }
+            return result;
 		}
 
 		private int? GetRandomAvailable()
@@ -107,6 +122,22 @@ namespace PlotterDuck.Components.Models.TicTacToe
 			}
             return null;
 		}
+
+        private int? GetBestAvailableFromList(int[] considered)
+        {
+			int[] available = considered.Where(x => Board.Boardstates[x] == TTTBoxState.Empty).ToArray();
+			if (available.Contains(4))
+            {
+                return 4;
+            }
+
+            available.Where(x => x % 2 == 0).ToArray();
+            Random rnd = new Random();
+            if(available.Length > 0)
+			    return available[rnd.Next(0, available.Length)];
+            else 
+                return null;
+        }
 
 		List<int> All1toWin(TTTBoxState state = TTTBoxState.O)
         {
