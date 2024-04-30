4e6e41220a9bf8620ecb155fb4a90fb094f5ddf9
Mirfean <mrfruzio@gmail.com>
2024-04-30 16:24:54 +0200

    Add form to authentication

---
+++ PlotterDuck/Components/Models/Firebase/AuthData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotterDuck.Components.Models.Firebase
+{
+    internal class AuthData
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
